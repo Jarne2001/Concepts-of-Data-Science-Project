f0bf2cf292f5a872da2b2acf0d34d9141580c450
Jarne2001 <jarnemelis01@gmail.com>
2025-05-03 15:03:54 +0200

    Create Ternary Search Tree.py
    The main file for the project for the ternary search tree is created.

---
+++ Ternary Search Tree.py
@@ -0,0 +1,6 @@
+# This Python file contains the code for the implementation of a Ternary Search Tree with an object-oriented approach.
+
+class(Ternary Search Tree):
+  def __init__(self, letter):
+    self.letter = letter
+    self.letter1 = None

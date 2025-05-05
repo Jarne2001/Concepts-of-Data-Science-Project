5095ec4760d8de9fbfe078119253aa4221359080
Jarne2001 <jarnemelis01@gmail.com>
2025-05-05 12:22:10 +0200

    Update Ternary Search Tree.py
    Add a more detailed description about writing the ternary search tree in big lines and some function suggestions.

--- Ternary Search Tree.py
+++ Ternary Search Tree.py
@@ -1,5 +1,8 @@
 # This Python file contains the code for the implementation of a Ternary Search Tree with an object-oriented approach.
 
+# I suggest making 2 classes: a Ternary Search Tree Node and a Ternary Search Tree with currently 3 functions: insert, search and delete (and
+# potentially also a function printing out the tree?)
+
 class(Ternary Search Tree):
   def __init__(self, letter):
     self.letter = letter

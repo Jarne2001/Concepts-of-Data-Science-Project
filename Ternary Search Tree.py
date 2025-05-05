38552d9f3438d9dfcea984818d63f0e9054ff9cb
Jarne2001 <jarnemelis01@gmail.com>
2025-05-05 14:04:50 +0200

    Add insert, all_strings and len functions
    Add the main base of the insert function, the all_strings and len functions. Also additional function all_strings in description above. Has yet to be tested.

--- Ternary Search Tree.py
+++ Ternary Search Tree.py
@@ -1,6 +1,6 @@
 # This Python file contains the code for the implementation of a Ternary Search Tree with an object-oriented approach.
 
-# I suggest making 2 classes: a Ternary Search Tree Node and a Ternary Search Tree with currently 3 functions: insert, search and delete (and
+# I suggest making 2 classes: a Ternary Search Tree Node and a Ternary Search Tree with currently 4 functions: insert, search, all_strings and delete (and
 # potentially also a function printing out the tree?)
 
 class(TernarySearchTreeNode):
@@ -17,9 +17,42 @@ class (TernarySearchTree):
       raise KeyError("No word is given!")
     index = 0
     node = None
-    word = list(word)
+    string_list = []
+    string_list.append(word)
     if self._root is None:
-      self._root == insert(self.root, word[0])
+      self._root == self.insert(self.root, word[index])
+      return
+    else:
+      letter = word[index]
+      if node == None:
+        node = TernarySearchTreeNode(letter)
+      while index < len(word):
+        if letter > node.letter:
+          if node.hi is None:
+            node.hi = self.insert(node.hi, letter)
+            index += 1
+        if letter < node.letter:
+          if node.lo is None:
+            node.lo = self.insert(node.lo, letter)
+            index += 1
+        if letter == node.letter:
+          if node.equal is None:
+            node.equal = self.insert(node.equal, letter)
+            index += 1
+        if index == len(word):
+          return node
+
+  def all_strings(self):
+    if not string_list:
+      return print("Tree is empty!")
+    return string_list
+
+  def len(self):
+    if not string_list:
+      return 0
+    return len(string_list)
+    
+    
       
       
       

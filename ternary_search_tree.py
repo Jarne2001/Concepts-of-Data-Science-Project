041e6588e37e2904e48f16933bcaa2cc64f12f2a
thomlaer <thomas.vanlaer@student.uhasselt.be>
2025-06-11 11:24:15 +0200

    fix: minor adjustment to make script run correctly

--- ternary_search_tree.py
+++ ternary_search_tree.py
@@ -248,7 +248,7 @@ def main():
         print(f"{case}: {insert_times[case]}")
     plt.figure()
     for case, times in insert_times.items():
-        plt.plot(list(time.keys()), list(time.values()), label=case)
+        plt.plot(list(times.keys()), list(time.values()), label=case)
     plt.title("Insert Performance: Best Case vs Average Case vs Worst Case")
     plt.xlabel("Tree size")
     plt.ylabel("Time (ms)")
@@ -278,7 +278,7 @@ def main():
 
     plt.figure()
     for case, times in search_times.items():
-        plt.plot(list(time.keys()), list(time.values()), label=case)
+        plt.plot(list(times.keys()), list(time.values()), label=case)
     plt.title("Search Performance: Best Case vs Average Case vs Worst Case")
     plt.xlabel("Tree size")
     plt.ylabel("Time (ms)")
